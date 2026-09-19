e35bdf97acaeb57e4393b0944245d9f079406bb2
Samar Khajuria <130741241+SUMMERxKx@users.noreply.github.com>
2026-09-19 12:55:38 -0700

    Add: duplicate-integer - submission-27

---
+++ Data Structures & Algorithms/duplicate-integer/submission-27.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        HashSet<Number> seen = new HashSet<>();
+
+        for(int num: nums){
+            if(seen.contains(num)){
+                return true;
+            }
+            else{
+                seen.add(num);
+            }
+        }
+        return false;
+        
+    }
+}
